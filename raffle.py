362e55663c255bc0e9d421dfc528c1e60a64e993
ldesgoui <ldesgoui@gmail.com>
2018-09-04 02:15:15 +0200

    another month another season

--- raffle.py
+++ raffle.py
@@ -48,14 +48,14 @@ def entries(hub, _, season, limit=50):
 if __name__ == "__main__":
     random.seed("TF2PL")
     hubs = [
-            ["d168a467-25a7-4eb4-92e9-d98501a15755", "NA Invite",   3],
-            ["dee30db4-5db8-4ed0-a9a6-74ebbd5c612f", "NA Advanced", 3],
-            ["3c265612-dd2a-4d1f-8cf4-c8a6c1d92472", "NA Amateur",  3],
-            ["b23dd05f-5876-454c-9a48-4e35e105ebd8", "NA Beginner", 3],
-            ["60166830-385b-4b26-acb5-4a6ba265623f", "EU Invite",   2],
-            ["abce6d7d-c317-425f-944d-14b9235aec5c", "EU Advanced", 2],
-            ["5dd0a523-dd96-420a-9c68-bbba6d0b1281", "EU Amateur",  2],
-            ["f76beaa7-768b-4158-9a3c-0ae2b8ce3295", "EU Beginner", 2],
+            ["d168a467-25a7-4eb4-92e9-d98501a15755", "NA Invite",   5],
+            ["dee30db4-5db8-4ed0-a9a6-74ebbd5c612f", "NA Advanced", 6],
+            ["3c265612-dd2a-4d1f-8cf4-c8a6c1d92472", "NA Open",  6],
+            ["b23dd05f-5876-454c-9a48-4e35e105ebd8", "NA Beginner", 6],
+            ["60166830-385b-4b26-acb5-4a6ba265623f", "EU Invite",   4],
+            ["abce6d7d-c317-425f-944d-14b9235aec5c", "EU Advanced", 4],
+            ["5dd0a523-dd96-420a-9c68-bbba6d0b1281", "EU Open",  5],
+            ["f76beaa7-768b-4158-9a3c-0ae2b8ce3295", "EU Beginner", 5],
         ]
     for hub in hubs:
         all_entries = list(entries(*hub))
